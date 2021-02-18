144b55e23dede694ae22c04c91026e347bb70567
HaoZijian <2425213725@qq.com>
2021-02-18 17:52:42 +0800

    修改 扫码签到功能

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,45 +1,63 @@
 Component({
   data: {
-  active: 0,
-  list: [
-    {
-      "url": "/pages/index/index",
-      "icon": "wap-home-o",
-      "text": "首页"
-    },
-    {
-      "icon": "scan",
-      "text": "扫码签到",
-      "flag": 1
-    },
-    {
-      "url": "/pages/me/me",
-      "icon": "user-circle-o",
-      "text": "我的"
-    }
-  ]
+    active: 0,
+    list: [{
+        "url": "/pages/index/index",
+        "icon": "wap-home-o",
+        "text": "首页"
+      },
+      {
+        "icon": "scan",
+        "text": "扫码签到",
+        "flag": 1
+      },
+      {
+        "url": "/pages/me/me",
+        "icon": "user-circle-o",
+        "text": "我的"
+      }
+    ],
+    aid: null
   },
   methods: {
-   onChange(e) {
-      this.setData({ active: e.detail });
-      if(this.data.list[e.detail].flag!=1){
+    onChange(e) {
+      this.setData({
+        active: e.detail
+      });
+      if (this.data.list[e.detail].flag != 1) {
         wx.switchTab({
           url: this.data.list[e.detail].url
         });
-      }else{
+      } else {
+        var that = this;
         wx.scanCode({
           onlyFromCamera: true,
-          success (res) {
+          success(res) {
             console.log(res)
+            that.setData({
+              aid: res.result
+            })
+            if (that.data.aid != null) {
+              wx.showModal({
+                title: '提示',
+                content: '签到成功!'
+              })
+            }
+          },
+          fail(res) {
+            wx.showModal({
+              title: '提示',
+              content: '未识别签到二维码!'
+            })
           }
         })
       }
-   },
-   init() {
-       const page = getCurrentPages().pop();
-       this.setData({
-      　  active: this.data.list.findIndex(item => item.url === `/${page.route}`)
-       });
-      }
-   }
+    },
+    init() {
+      const page = getCurrentPages().pop();
+      this.setData({
+        active: this.data.list.findIndex(item => item.url === `/${page.route}`)
+      });
+    }
+  }
 });
