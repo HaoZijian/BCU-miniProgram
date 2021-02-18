10319f5b21a6ef510568e18fb2875fb979cde766
Mecho <i@obj.me>
2021-02-18 15:09:14 +0800

    成功唤起二维码扫描

--- custom-tab-bar/index.js
+++ custom-tab-bar/index.js
@@ -1,65 +1,45 @@
 Component({
   data: {
-    active: 0,
-    list: [{
-        "url": "/pages/index/index",
-        "icon": "wap-home-o",
-        "text": "首页",
-      },
-      {
-        "icon": "scan",
-        "text": "扫码签到",
-        "flag": 1
-      },
-      {
-        "url": "/pages/me/me",
-        "icon": "user-circle-o",
-        "text": "我的",
-      }
-    ],
-    aid: null
+  active: 0,
+  list: [
+    {
+      "url": "/pages/index/index",
+      "icon": "wap-home-o",
+      "text": "首页"
+    },
+    {
+      "icon": "scan",
+      "text": "扫码签到",
+      "flag": 1
+    },
+    {
+      "url": "/pages/me/me",
+      "icon": "user-circle-o",
+      "text": "我的"
+    }
+  ]
   },
   methods: {
-    onChange(e) {
-      console.log(e, 'e')
-      this.setData({
-        active: e.detail
-      });
-      if (this.data.list.flag != 1) {
+   onChange(e) {
+      this.setData({ active: e.detail });
+      if(this.data.list[e.detail].flag!=1){
         wx.switchTab({
           url: this.data.list[e.detail].url
         });
-      }
-      else if (this.data.list.flag == 1) {
+      }else{
         wx.scanCode({
           onlyFromCamera: true,
-          success: res => {
+          success (res) {
             console.log(res)
-            this.setData({
-              aid: res.result
-            })
-            if(this.data.aid != null) {
-              wx.showModal({
-                title: '提示',
-                content: '签到成功！'
-              })
-            }
-          },
-          fail: res => {
-            console.log(res)
-            wx.showModal({
-              title: '提示',
-              content: '未识别签到二维码!'
-            })
           }
         })
       }
-    },
-    init() {
-      const page = getCurrentPages().pop();
-      this.setData({
-        active: this.data.list.findIndex(item => item.url === `/${page.route}`)
-      });
-    }
-  }
+   },
+   init() {
+       const page = getCurrentPages().pop();
+       this.setData({
+      　  active: this.data.list.findIndex(item => item.url === `/${page.route}`)
+       });
+      }
+   }
 });
