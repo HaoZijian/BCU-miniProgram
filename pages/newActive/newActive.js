7b7d08ef45a79baabd0687fa8aa575d8407716fc
HaoZijian <2425213725@qq.com>
2021-02-19 23:10:47 +0800

    新增 活动发布页面
    同时修改一些小问题

---
+++ pages/newActive/newActive.js
@@ -0,0 +1,141 @@
+// pages/newActive/newActive.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    fileList: [],
+    value: '',
+    show: false,
+    currentDate: new Date().getTime(),
+    minDate: new Date().getTime(),
+    formatter(type, value) {
+      if (type === 'year') {
+        return `${value}年`;
+      } else if (type === 'month') {
+        return `${value}月`;
+      }
+      return value;
+    },
+  },
+
+  onConfirm() {
+    this.setData({
+      show: false
+    });
+  },
+
+  onCancel() {
+    this.setData({
+      show: false
+    });
+  },
+
+  onInput(event) {
+    this.setData({
+      currentDate: event.detail,
+    });
+  },
+
+  showPopup() {
+    this.setData({
+      show: true
+    });
+  },
+
+  onClose() {
+    this.setData({
+      show: false
+    });
+  },
+
+  onChange(event) {
+    // event.detail 为当前输入的值
+    console.log(event.detail);
+  },
+
+  afterRead(event) {
+    const {
+      file
+    } = event.detail;
+    // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
+    wx.uploadFile({
+      url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
+      filePath: file.url,
+      name: 'file',
+      formData: {
+        user: 'test'
+      },
+      success(res) {
+        // 上传完成需要更新 fileList
+        const {
+          fileList = []
+        } = this.data;
+        fileList.push({
+          ...file,
+          url: res.data
+        });
+        this.setData({
+          fileList
+        });
+      },
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
